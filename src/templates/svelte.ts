4dcd4eec9d2e64f82728b47068408c438af971d5
halvaradop_ <hernanvid123@gmail.com>
2024-06-23 18:43:38 -0500

    refactor: improve initilization codebase

--- src/templates/svelte.ts
+++ src/templates/svelte.ts
@@ -1,8 +1,12 @@
 
-export const codeSvelteKit = () => `
+export const codeSvelteKitBase = `
 import { SvelteKitAuth } from "@auth/sveltekit"
  
 export const { handle } = SvelteKitAuth({
     providers: [],
 })
+`
+
+export const codeSvelteKitHandler = `
+export { handle } from "./auth"
 `
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path from "path"
 import { promisify } from "util"
 import { exec } from "child_process"
@@ -9,3 +10,49 @@ import { exec } from "child_process"
  */
 export const execAsync = promisify(exec)
 export const ROOT = path.resolve(process.cwd())
+
+
+/**
+ * Constructs a path by joining the ROOT directory with the specified route.
+ * 
+ * This function takes an optional route (`route`) and returns the full path 
+ * by joining the ROOT directory with the given route. If no route is provided, 
+ * it returns the ROOT directory.
+ * 
+ * @param {string} route The optional route to be appended to the ROOT directory.
+ * @returns {string} The full path constructed by joining the ROOT directory and the route.
+ */
+export const configPath = (route: string = ""): string => {
+    return path.join(ROOT, route)
+}
+
+
+/**
+ * Creates the specified path and writes the content to the file.
+ * 
+ * This function takes a path (`route`) and content (`content`), creates all necessary directories 
+ * in the specified path, and if the last element in the path is a file (with .js or .ts extension), 
+ * writes the content to the file.
+ * 
+ * @param {string} route - The full path where the directories and file should be created.
+ * @param {string} content - The content to be written to the file.
+ */
+export const writeConfig = (route: string, content: string): void => {
+    let root = configPath()
+    const relative = path.relative(ROOT, route).split("\\")
+    relative.forEach(routePath => {
+        root = path.join(root, routePath)
+        if(!fs.existsSync(root)) {
+            if(!routePath.match(".(js|ts)")) {
+                if(!fs.existsSync(root)) {
+                    fs.mkdirSync(root, { recursive: true })
+                }
+            } else {
+                fs.writeFileSync(route, content, {
+                    flag: "w",
+                    encoding: "utf-8"
+                })
+            }
+        }
+    })
+}

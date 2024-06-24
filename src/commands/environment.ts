f64b8fa8d79ffd4c7e63221ec7383118498af15b
halvaradop_ <hernanvid123@gmail.com>
2024-06-23 21:51:54 -0500

    feat: add module to setup env for providers

--- src/commands/environment.ts
+++ src/commands/environment.ts
@@ -1,7 +1,5 @@
-import fs from "fs"
-import path from "path"
 import { createSpinner } from "nanospinner"
-import { execAsync, ROOT } from "../utils.js"
+import { execAsync, setEnvironment } from "../utils.js"
 
 
 /**
@@ -9,21 +7,13 @@ import { execAsync, ROOT } from "../utils.js"
  * the AUTH_SECRET variable. This is mandatory for using auth.js without considering
  * the framework.
  */
-export const setEnvironment = async (): Promise<void> => {
-    const environmentPath = path.join(ROOT, ".env")
-    const existVariable = process.env.AUTH_SECRET ?? process.env.NEXT_AUTH
-
-    if(!existVariable) {
-        const spinner = createSpinner("Setting up the environment variables of the project").start()
-        try {
-            const randomized = await getRandonSecret()
-            fs.writeFileSync(environmentPath, `AUTH_SECRET=${randomized}`)
-            spinner.success({ text: "The AUTH_SECRET variable was created" })
-        } catch(error) {
-            spinner.error({ text: "An error occurred while generating the secret key" })
-        }
+export const setAuthConfigEnvironment = async (): Promise<void> => {
+    try {
+        const randomized = await getRandonSecret()
+        setEnvironment("AUTH_SECRET", randomized)
+    } catch(error) {
+        createSpinner("Error").error({ text: "An error occurred while generating the secret key" })
     }
-    createSpinner("The AUTH_SECRENT already exists").warn()
 }
 
 
@@ -33,7 +23,7 @@ export const setEnvironment = async (): Promise<void> => {
  * 
  * @returns {Promise<string>} The generated secret key
  */
-const getRandonSecret = async () => {
+export const getRandonSecret = async (): Promise<string> => {
     const { stdout } = await execAsync("openssl rand -base64 33")
     return stdout
 }

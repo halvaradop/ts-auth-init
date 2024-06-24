c9b9bb32ef0841fe4ac655e9c0eb539e89c23841
halvaradop_ <hernanvid123@gmail.com>
2024-06-23 19:35:47 -0500

    chore: create prompts folder

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,28 @@
 #! /usr/bin/env node
 
 import "dotenv/config.js"
-import { select, confirm, input } from "@inquirer/prompts"
-import { FlagOptions, Framework } from "./types.js"
-import { initializeAuth } from "./commands/auth.js"
+import { FlagOptions } from "./types.js"
 import { Command } from "commander"
 import { setEnvironment } from "./commands/environment.js"
+import { promptInitConfig } from "./prompts/init.js"
 
-const framework = await select<Framework>({
-    message: "Select the framework that you will use in your project",
-    choices: [
-        { name: "NextJs", value: "NextJs" },
-        { name: "SvelteKit", value: "SvelteKit" },
-        { name: "Express", value: "Express" },
-    ]
-})
 
-const configuration = await confirm({
-    message: "You want to create file configuration ?",
-})
-
-const fileName = await input({
-    message: "Name of the configuration file",
-    default: "auth.ts"
-})
-
+/**
+ * Declare and initialize the program
+ */
 const program = new Command()
 
+/**
+ * 
+ */
 program
     .name("auth-init")
     .description("Initializer a project with Auth.js")
     .version("0.0.1")
 
+/**
+ * Configuration of CLI options and arguments
+ */
 program
     .option("-s, --secret", "Generate the secret key")
     .action(async (flags: FlagOptions) => {
@@ -40,5 +31,8 @@ program
         }
     })
 
+/**
+* Parse the command line arguments
+*/
 program.parseAsync(process.argv)
-initializeAuth(framework, configuration, fileName)
+promptInitConfig()

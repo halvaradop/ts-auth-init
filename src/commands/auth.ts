71e1f7344b3854192ea04110277bfd591709af7f
halvaradop_ <hernanvid123@gmail.com>
2024-06-23 19:11:48 -0500

    refactor: improve the code and documentation

--- src/commands/auth.ts
+++ src/commands/auth.ts
@@ -1,21 +1,26 @@
 import { createSpinner } from "nanospinner"
 import { execAsync, writeConfig } from "../utils.js"
 import { Framework } from "../types.js"
-import { frameworkCode, frameworkInstall } from "./frameworks.js"
-
+import { frameworkInstall, getCodeByFramework } from "./frameworks.js"
 
 
 /**
- * Initialize the set of configuration for a framework, like:
+ * Initializes the set of configurations for a specified framework, such as:
  *  - NextJs
  *  - SvelteKit
  *  - Express
+ *
+ * This function installs the necessary authentication dependencies for the chosen framework
+ * and optionally creates configuration files.
  * 
- * @param framework the configuration of the framework
+ * @param {Framework} framework - The framework to initialize (NextJs, SvelteKit, Express).
+ * @param {boolean} create - Whether to create configuration files.
+ * @param {string} fileName - The name of the configuration file to be created.
+ * @returns {Promise<void>} A promise that resolves when the initialization is complete.
  */
-export const initializeAuth = async (framework: Framework, create: boolean, fileName: string) => {
+export const initializeAuth = async (framework: Framework, create: boolean, fileName: string): Promise<void> => {
     installFrameworkAuth(framework)
-    const configFramework = frameworkCode[framework]
+    const configFramework = getCodeByFramework(framework, fileName)
     if (create) {
         configFramework.map(({ path, content }) => writeConfig(path, content))
     }
@@ -23,10 +28,10 @@ export const initializeAuth = async (framework: Framework, create: boolean, file
 
 
 /**
- * Installs the necessary dependencies for the selected framework
- * from the terminal.
+ * Installs the necessary dependencies for the selected framework from the terminal.
  * 
- * @param framework The framework chosen by the user
+ * @param {Framework} framework - The framework chosen by the user (NextJs, SvelteKit, Express).
+ * @returns {Promise<void>} A promise that resolves when the installation is complete.
  */
 export const installFrameworkAuth = async (framework: Framework): Promise<void> => {
     const install = frameworkInstall[framework]

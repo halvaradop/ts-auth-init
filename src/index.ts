01671f09b0db3fcdd60520a00dc3bdb8ba8bd01c
halvaradop_ <hernanvid123@gmail.com>
2024-06-23 22:05:05 -0500

    chore(release): published the 0.0.2 version on npm

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { FlagOptions } from "./types.js"
 import { Command } from "commander"
 import { setAuthConfigEnvironment } from "./commands/environment.js"
 import { promptInitProviders } from "./prompts/providers.js"
+import { promptInitConfig } from "./prompts/init.js"
 
 
 /**
@@ -39,4 +40,4 @@ program
 * Parse the command line arguments
 */
 program.parseAsync(process.argv)
-//promptInitConfig()
+await promptInitConfig()
